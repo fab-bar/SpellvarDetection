bb2b77a777f64f6e7b3936fb457a7739d082af72
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2018-08-20 17:41:31 +0200

    Fix typo in test for Levenshtein generator

--- spellvardetection/test/generator/test_levenshtein_generator.py
+++ spellvardetection/test/generator/test_levenshtein_generator.py
@@ -6,7 +6,7 @@ class TestLevenshteinGenerator(unittest.TestCase):
 
     def test_getCandidates(self):
 
-        generator = LevenshteinGenerator(['cat', 'mat', 'hat', 'dog' 'apple', 'flat'], 1)
+        generator = LevenshteinGenerator(['cat', 'mat', 'hat', 'dog', 'apple', 'flat'], 1)
         self.assertEqual(generator.getCandidatesForWords(['rat', 'dog']), {'rat': set(['cat', 'mat', 'hat']), 'dog': set()})
-        generator = LevenshteinGenerator(['cat', 'mat', 'hat', 'dog' 'apple', 'flat'], 2)
+        generator = LevenshteinGenerator(['cat', 'mat', 'hat', 'dog', 'apple', 'flat'], 2)
         self.assertEqual(generator.getCandidatesForWords(['rat', 'dog']), {'rat': set(['cat', 'mat', 'hat', 'flat']), 'dog': set()})
