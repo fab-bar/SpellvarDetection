146d498717238fd5ad3fe723d7f0459f5701fc57
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2018-09-15 20:38:43 +0200

    Use version from git tag

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='SpellvarDetection',
-      version='0.1.0',
+      use_scm_version=True,
       url='https://github.com/fab-bar/SpellvarDetection',
       author='Fabian Barteld',
       author_email='fabian.barteld@rub.de',
       license='MIT',
       packages=find_packages(),
+      setup_requires=['setuptools_scm'],
       install_requires=[
           'numpy<1.15',
           'scipy',
